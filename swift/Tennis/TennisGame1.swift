ce02edb846c5b9d6b576834e205a05cac64001ec
Ivan Rublev <ivan@ivanrublev.me>
2017-10-08 14:02:25 +0300

    Do the same with naming methods for greater and less then four score.

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -33,47 +33,10 @@ class TennisGame1: TennisGame {
             return nameOfEqualScore()
         }
         else if score1 >= 4 || score2 >= 4 {
-            let minusResult = score1-score2
-            if minusResult == 1 {
-                score = "Advantage player1"
-            }
-            else if minusResult == -1 {
-                score = "Advantage player2"
-            }
-            else if minusResult >= 2 {
-                score = "Win for player1"
-            }
-            else {
-                score = "Win for player2"
-            }
+            return nameOfGreaterThenFourScore()
         }
         else {
-            var tempScore = 0
-            for i in 1..<3 {
-                if i == 1 {
-                    tempScore = score1
-                }
-                else {
-                    score = "\(score)-"
-                    tempScore = score2
-                }
-                switch tempScore {
-                case 0:
-                    score = "\(score)Love"
-
-                case 1:
-                    score = "\(score)Fifteen"
-
-                case 2:
-                    score = "\(score)Thirty"
-
-                case 3:
-                    score = "\(score)Forty"
-
-                default:
-                    break
-                }
-            }
+            return nameOfLessThenFourScore()
         }
         return score
     }
@@ -92,6 +55,55 @@ class TennisGame1: TennisGame {
             
         default:
             score = "Deuce"
+        }
+        return score
+    }
+    
+    private func nameOfGreaterThenFourScore() -> String {
+        var score = ""
+        let minusResult = score1-score2
+        if minusResult == 1 {
+            score = "Advantage player1"
+        }
+        else if minusResult == -1 {
+            score = "Advantage player2"
+        }
+        else if minusResult >= 2 {
+            score = "Win for player1"
+        }
+        else {
+            score = "Win for player2"
+        }
+        return score
+    }
+    
+    private func nameOfLessThenFourScore() -> String {
+        var score = ""
+        var tempScore = 0
+        for i in 1..<3 {
+            if i == 1 {
+                tempScore = score1
+            }
+            else {
+                score = "\(score)-"
+                tempScore = score2
+            }
+            switch tempScore {
+            case 0:
+                score = "\(score)Love"
+                
+            case 1:
+                score = "\(score)Fifteen"
+                
+            case 2:
+                score = "\(score)Thirty"
+                
+            case 3:
+                score = "\(score)Forty"
+                
+            default:
+                break
+            }
         }
         return score
     }
