b6432e48d70de38880aa3cdecd7943a30a0c07a4
Ivan Rublev <ivan@ivanrublev.me>
2017-10-08 23:20:55 +0300

    GamePhaseAfter4Point returns correct scores description now. All tests are passed.

--- swift/Tennis/GamePhaseAfter4Point.swift
+++ swift/Tennis/GamePhaseAfter4Point.swift
@@ -10,13 +10,31 @@ import Foundation
 class GamePhaseAfter4Point: GamePhase {
     private let player1: Player
     private let player2: Player
+    private let score1: Score
+    private let score2: Score
     
     init(player1: Player, player2: Player) {
         self.player1 = player1
         self.player2 = player2
+        self.score1 = player1.score
+        self.score2 = player2.score
     }
     
     var scoresDescription: String {
-        return ""
+        let advantage = advantageName()
+        let leadingPlayer = leadingPlayerName()
+        let score = "\(advantage) \(leadingPlayer)"
+        return score
+    }
+    
+    private func advantageName() -> String {
+        let name = score1.advantageName(for: score2) ?? ""
+        return name
+    }
+    
+    private func leadingPlayerName() -> String {
+        let leadingPlayer = Player.leader(player1: player1, player2: player2)
+        let name = leadingPlayer?.name ?? ""
+        return name
     }
 }
