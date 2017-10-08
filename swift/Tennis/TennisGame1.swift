b8c334e3a551f9334a7f9afcd8509c6611e3f969
Ivan Rublev <ivan@ivanrublev.me>
2017-10-08 16:52:54 +0300

    We made array of scores to remove if in wonPoint().

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -12,19 +12,20 @@ class TennisGame1: TennisGame {
     private let player2: String
     private var score1 = Score()
     private var score2 = Score()
+    private let scores: [String : Score]
     
     required init(player1: String, player2: String) {
         self.player1 = player1
         self.player2 = player2
+
+        var scores = [String : Score]()
+        scores[player1] = score1
+        scores[player2] = score2
+        self.scores = scores
     }
 
     func wonPoint(_ playerName: String) {
-        if playerName == "player1" {
-            score1.plusPoint()
-        }
-        else {
-            score2.plusPoint()
-        }
+        scores[playerName]?.plusPoint()
     }
     
     var score: String? {
